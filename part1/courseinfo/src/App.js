42defa43261fd62166d6770fffffa83d7d03273a
unknown <hlololo040@gmail.com>
2023-08-08 14:36:06 +0700

    fix

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,20 @@
 
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>
-}
+const Header = ({ course }) => <h1>{course.name}</h1>
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part parts={props.parts[0]} />
-      <Part parts={props.parts[1]} />
-      <Part parts={props.parts[2]} />
-    </div>
-  )
-}
-const Part = (props) => {
-  return <p>{props.parts.name} {props.parts.exercises}</p>
-}
+const Content = ({ parts }) => 
+  <div>
+    <Part parts={parts[0]} />
+    <Part parts={parts[1]} />
+    <Part parts={parts[2]} />
+  </div>
 
-const Total = (props) => {
-  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+
+const Total = ({ parts }) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return <p>Number of exercises {total}</p>
 }
-
 
 
 const App = () => { 
